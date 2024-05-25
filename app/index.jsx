7fd8c66313004c21244704271a1ca38816575f01
navyumm <navyum12@gmail.com>
2024-05-25 22:32:24 +0530

    "Updated auth layout and sign-in page with new UI components and routing"

--- app/index.jsx
+++ app/index.jsx
@@ -9,28 +9,35 @@ import CustomButton from "../components/CustomButton";
 export default function App() {
     return (
         <SafeAreaView className="bg-primary h-full">
-            <ScrollView contentContainerStyle={{ height: "100%" }}>
-                <View className="w-full justify-center items-center min-h-[95vh] px-4">
 
+            <ScrollView
+                contentContainerStyle={{
+                    height: "100%",
+                }}
+            >
+                <View className="w-full flex justify-center items-center min-h-[90vh] px-4">
                     <Image
                         source={images.logo}
-                        className="w-[130px] h-[40px]"
+                        className="w-[130px] h-[84px]"
                         resizeMode="contain"
                     />
+
                     <Image
                         source={images.cards}
                         className="max-w-[380px] w-full h-[298px]"
                         resizeMode="contain"
                     />
 
-                    <View>
-                        <Text className="text-white text-3xl font-bold text-center">Discover Endless Posibilities with
-                            <Text className="text-secondary-200"> Aora</Text>
+                    <View className="relative mt-5">
+                        <Text className="text-3xl text-white font-bold text-center">
+                            Discover Endless{"\n"}
+                            Possibilities with{" "}
+                            <Text className="text-secondary-200">Aora</Text>
                         </Text>
 
                         <Image
                             source={images.path}
-                            className="w-[130px] h-[15px] absolute -bottom-1 -right-8"
+                            className="w-[136px] h-[14px] absolute -bottom-1 -right-9"
                             resizeMode="contain"
                         />
                     </View>
@@ -40,17 +47,16 @@ export default function App() {
                         Exploration with Aora
                     </Text>
 
-                    <CustomButton 
+                    <CustomButton
                         title="Continue with Email"
-                        handlePress={()=> router.push('./sign-in')}
+                        handlePress={() => router.push("/sign-in")}
                         containerStyles="w-full mt-7"
                     />
-
                 </View>
             </ScrollView>
 
             <StatusBar backgroundColor="#161622"
-            style="light"
+                style="light"
             />
 
         </SafeAreaView>
