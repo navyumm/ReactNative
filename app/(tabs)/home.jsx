16c110f4dfadf239130379bd36c8a79adc15f3b1
navyumm <navyum12@gmail.com>
2024-06-10 11:44:30 +0530

    "Updated Home screen to fetch posts using useAppwrite hook, added VideoCard component, and refactored Trending component to display videos with animations and playback functionality."

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -6,48 +6,37 @@ import { images } from "../../constants";
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
-import { getAllPosts } from '../../lib/appwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
+import useAppwrite from '../../lib/useAppwrite';
+import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-
-            try {
-                const reponse = await getAllPosts();
-
-                setData(reponse);
-            } catch (error) {
-                Alert.alert('Error', error.message)
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchData();
-    }, [])
-    
-    console.log(data);
+    const { data: posts, refetch } = useAppwrite(getAllPosts);
+    const { data: latestPosts } = useAppwrite(getLatestPosts);
 
     const [refreshing, setRefreshing] = useState(false);
 
     const onRefresh = async () => {
         setRefreshing(true);
         // re call videos -> if any new videos appeard
+        await refetch();
         setRefreshing(false);
     }
 
+    // console.log(posts);
 
     return (
         <SafeAreaView className="bg-primary h-full">
             <FlatList
-                data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+                data={posts}
                 // data={[]}
                 keyExtractor={(item) => item.$id}
                 renderItem={({ item }) => (
-                    <Text className="text-3xl text-white">{item.id}</Text>
+                    // <Text className="text-3xl text-white">{item.title}</Text>
+                    <VideoCard 
+                        video={item}
+                    />
                 )}
                 ListHeaderComponent={() => (
                     <View className="my-6 px-4 space-y-6">
@@ -77,7 +66,7 @@ const Home = () => {
                                 Latest Videos
                             </Text>
 
-                            <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+                            <Trending posts={ latestPosts ?? []} />
 
                         </View>
                     </View>
